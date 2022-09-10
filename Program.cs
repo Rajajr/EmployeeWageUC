5815bfa672d0568e3ede2bc669608650ab7c090d
Rajajr <rajjr011@gmail.com>
2022-09-11 02:52:44 +0700

    UC-2 Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -10,17 +10,29 @@ namespace EmployeeWageCompte1958Batch
     {
         static void Main(string[] args)
         {
-            //UC-1 Find Employee is Present or not
+            //UC-2 Find Daily Wage Of Employee
             int empPresent = 1;
+            int EMP_RATE_PER_HOUR = 40;
+
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Generate random number within range either 0 or 1
             int empcheck = random.Next(0, 2);
             Console.WriteLine("Random value:" + empcheck);
 
-            if (empPresent == empcheck)
-                Console.WriteLine("Employee is Present");
+            if (empcheck==empPresent)
+            {
+                empHrs = 10;
+            }
+               
             else
-                Console.WriteLine("Employee is Absent");
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage is:"+empWage);
             Console.ReadLine();
         }
     }
