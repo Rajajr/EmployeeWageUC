8b14d257d032dbdcf8a2b615ed1e97f4d09fb978
Rajajr <rajjr011@gmail.com>
2022-09-12 10:59:36 +0700

    Add Part time Employee & Wage

--- Program.cs
+++ Program.cs
@@ -10,26 +10,31 @@ namespace EmployeeWageCompte1958Batch
     {
         static void Main(string[] args)
         {
-            //UC-2 Find Daily Wage Of Employee
-            int empPresent = 1;
+            //UC-3 Part time Employee & Wage
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 40;
-
-            //variables
-            int empHrs = 0;
-            int empWage = 0;
+            int empHrs = 0; int empWage = 0;
             Random random = new Random();
             //Generate random number within range either 0 or 1
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             Console.WriteLine("Random value:" + empcheck);
 
-            if (empcheck==empPresent)
+            if (IS_FULL_TIME == empcheck)
             {
                 empHrs = 10;
+                Console.WriteLine("Full Time Employee is Present");
             }
-               
+
+            else if (IS_PART_TIME == empcheck)
+            {
+                empHrs = 4;
+                Console.WriteLine("Part Time Employee is Present");
+            }
             else
             {
                 empHrs = 0;
+                Console.WriteLine("Employe is Absent");
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage is:"+empWage);
