f924dd4eb1a25eeb3d7843618bc53378a863227d
Rajajr <rajjr011@gmail.com>
2022-09-21 11:10:34 +0700

    Write a Class Method to Compute EmployeWage

--- Program.cs
+++ Program.cs
@@ -10,11 +10,16 @@ namespace EmployeeWageCompte1958Batch
     {
         static void Main(string[] args)
         {
-            //UC-6 Calculate Wages till a Condition of total working hours or max days os reached for a month
+            CaluculatingEmployeeWages();
+            Console.ReadLine();
+        }
+        public static void CaluculatingEmployeeWages()
+        {
+            //UC-7 Refactor the Code to write a Class Method to ComputeEmployee Wage
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            const int MAX_OF_WORKING_DAYS = 20;
+            const int MAX_OF_WORKING_DAYS = 30;
             const int MAX_WORKING_HRS = 100;
             //Variables
             int empHrs = 0,empWage = 0,totalEmpWage = 0,day = 1,totalHrs = 0;
@@ -22,7 +27,7 @@ namespace EmployeeWageCompte1958Batch
             //Generate random number within range either 0 or 1
             while (day <= MAX_OF_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
-                int empcheck = random.Next(0, 3);
+                var empcheck = random.Next(0, 3);
                 //Console.WriteLine("Random value:" + empcheck);
 
                 switch (empcheck)
@@ -38,22 +43,20 @@ namespace EmployeeWageCompte1958Batch
                         break;
 
                     default:
+                        empHrs = 0;
                         //Console.WriteLine("Employee is Absent");
                         break;
                 }
 
                 empWage = empHrs * EMP_RATE_PER_HR;
-                Console.WriteLine("Employee Wage Per Day{0} and Hrs: {0} is: {2}", day, totalHrs, empWage);
+                Console.WriteLine("Employee Wage Per Day is {0} and Hrs: {0} is: {2}", day, totalHrs, empWage);
                 totalEmpWage += empWage;
                 day++;
                 totalHrs += empHrs;
             }
 
-          
-          Console.WriteLine("Total Employe Wage is: " + totalEmpWage);
-          Console.ReadLine();
-
-           
+                Console.WriteLine("\nTotal Employee Wage of the month is: " + totalEmpWage);
+         
         }
 
     }
