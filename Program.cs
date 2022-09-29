637a369d9765bb6e643af54effde5d03f4e921db
Rajajr <rajjr011@gmail.com>
2022-09-29 09:19:04 +0700

    UC8 Employee Wage For Multiple Companies

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,21 +11,25 @@ namespace EmployeeWageCompte1958Batch
     {
         static void Main(string[] args)
         {
-            CaluculatingEmployeeWages();
+            // UC - 8 Compute Employe Wage For Multiple Companies
+            CaluculatingEmployeeWages("Google",20,30,120);
+            CaluculatingEmployeeWages("Microsoft", 20, 30,150 );
+            CaluculatingEmployeeWages("Infosys", 20, 30, 120);
             Console.ReadLine();
         }
-        public static void CaluculatingEmployeeWages()
+        public static void CaluculatingEmployeeWages(string COMPANY, int EMP_RATE_PER_HR, int MAX_OF_WORKING_DAYS,int MAX_WORKING_HRS)
         {
-            //UC-7 Refactor the Code to write a Class Method to ComputeEmployee Wage
+            
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
-            const int EMP_RATE_PER_HR = 20;
-            const int MAX_OF_WORKING_DAYS = 30;
-            const int MAX_WORKING_HRS = 100;
+            //const int EMP_RATE_PER_HR = 20;  
+            //const int MAX_OF_WORKING_DAYS = 30;
+            //const int MAX_WORKING_HRS = 100;
+
             //Variables
             int empHrs = 0,empWage = 0,totalEmpWage = 0,day = 1,totalHrs = 0;
             Random random = new Random();
-            //Generate random number within range either 0 or 1
+
             while (day <= MAX_OF_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 var empcheck = random.Next(0, 3);
@@ -49,13 +54,16 @@ namespace EmployeeWageCompte1958Batch
                 }
 
                 empWage = empHrs * EMP_RATE_PER_HR;
-                Console.WriteLine("Employee Wage Per Day is {0} and Hrs: {0} is: {2}", day, totalHrs, empWage);
+                //Console.WriteLine("Company Name: {0}",COMPANY);
+                //Console.WriteLine("Employee Wage Per Day is {0} and Hrs: {0} is: {2}", day, totalHrs, empWage);
                 totalEmpWage += empWage;
                 day++;
                 totalHrs += empHrs;
             }
 
-                Console.WriteLine("\nTotal Employee Wage of the month is: " + totalEmpWage);
+            Console.WriteLine("\nCompany Name: {0}", COMPANY);
+            Console.WriteLine("\nTotal Employee Wage of the month is: " + totalEmpWage);
+            
          
         }
 
